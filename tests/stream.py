d0a5d48888a0bff934a3f826dc74a5b3dd12066a
sambelltacq <Sam.Bell@d-tacq.co.uk>
2024-05-24 16:32:29 +0100

    Now uses free running waveform

--- tests/stream.py
+++ tests/stream.py
@@ -3,7 +3,7 @@
 """Tests uuts streaming"""
 
 from acq400_regression.BaseTest import BaseTest
-
+from acq400_regression.misc import tri
 
 class StreamTest(BaseTest):
     test_type = "stream"
@@ -15,37 +15,62 @@ class StreamTest(BaseTest):
         """Test specific arguments"""
         parser.add_argument('--siggen',  help='signal generator hostname', required=True)
         parser.add_argument('--runtime', default=5, type=int, help=f"stream runtime")
+        parser.add_argument('--period', default=0.2, type=float, help=f"override burst period")
+        parser.add_argument('--triggers', default='all', type=parser.list_of_trinarys, help='Triggers to test 1,0,0/1,0,1/1,1,1 or all')
         return parser
 
     def run(self):
         self.save_state('runtime', self.args.runtime)
         self.wavelength = self.args.wavelength #add into args
         
+        clk = [uut.clk for uut in self.uuts][0] #TODO make nicer
+        tlen = self.wavelength * self.args.cycles
+        self.period = round(tlen / clk * 3, 2)
+        print(f"period {self.period}")
+        
         freq, voltage = self.get_freq_and_voltage()
-        self.siggen.config_params(freq, voltage)
-        self.siggen.config_contiguous()
         
-        self.uuts.setup(trg='1,1,1')
+        
+        for trigger in self.get_trigger():
+            trigger = '1,0,1' #decide test triggers
+            
+            self.siggen.config_params(freq, voltage)
+            
+            
+            self.uuts.setup(trg=trigger)
 
-        self.run_iters()
-        
-        if self.args.save: self.th.save_test()
-        self.th.stash_results()
+            self.run_iters(trigger)
+            
+            if self.args.save: self.th.save_test()
+            self.th.stash_results()
         
         self.log.info(f"[{self.th.testname.title()}] Test Completed")
         
-    def run_iters(self):
+    def run_iters(self, trigger):
         
         for run in self.get_run():
             results = []
+            
             self.uuts.abort()
             
             self.log.info(f"Streaming to host for {self.args.runtime}s")
             
-            self.uuts.stream_to_host(runtime=self.args.runtime)
+            
+            self.siggen.enable_free_running_burst(self.args.cycles, self.period)
+            self.uuts.init_stream_to_host(runtime=self.args.runtime)
+        
+            self.uuts.wait_armed()
+            self.log.info('Ready')
+            
+            if tri(trigger, 'source') != 1: self.siggen.start_free_running_burst()
+            
+            self.uuts.wait_completed()
+            self.log.info('Ready')
             
             dataset = self.th.import_dataset('HOST')
             results.append(self.check_spad(dataset))
+            
+            #TODO: check waveform here
             
             if not self.check_passed(results): break
         self.log.info(f"All runs complete {self.run}/{self.runs}") 
